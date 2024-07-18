73599ab6f52b77d292164641cc3c6c38169b0c17
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2024-07-18 15:20:23 -0400

    state file

---
+++ day-4/main.tf
@@ -0,0 +1,15 @@
+//without using remote backend
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "naga" {
+  instance_type = "t2.micro"
+  ami = "abc123" //this is not actual aws ami, we have to provide actual aws ami here
+  subnet_id = "subnet-123" //this is not actual subnet id, we have to provide actual aws subnet id here
+}
+
+//execute this file using 1.terraform init 2.terraform plan(optional) 3.terraform apply
+//then you will get the state file in the project folder
+//delete the state file
+
